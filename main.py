909f944bdb2d9962c229fab69fea3a2cbbcb9c3b
Matthew Ridley <56508206+StolenGalaxy@users.noreply.github.com>
2025-08-07 15:11:11 +0100

    Began work on V2 of the viewbot

--- main.py
+++ main.py
@@ -1,44 +1,31 @@
+from time import sleep
 import undetected_chromedriver as uc
 from undetected_chromedriver import ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 
-identifiers = {
-    "views": "btn.btn-primary.rounded-0.t-views-button",
-    "urlBox": '//div[@class="col-sm-5 col-xs-12 p-1 container t-views-menu"]//input[@placeholder]',
-    "urlButton": '//div[@class="col-sm-5 col-xs-12 p-1 container t-views-menu"]//button',
-    "submitButton": '//div[@class="col-sm-5 col-xs-12 p-1 container t-views-menu"]//button[contains(@class, "wbutton")]'
-}
+options = ChromeOptions()
+
+options.binary_location = r"chrome-win64\chrome.exe"
+options.add_argument(r"--load-extension=adblock")
+options.add_argument('--disable-notifications')
+options.add_experimental_option(
+    "prefs", {
+        "profile.default_content_setting_values.notifications": 2
+    }
+)
 
 
-class Zefoy:
-    def __init__(self, videoURL):
-        self.url = videoURL
-        
-        options = ChromeOptions()
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        self.driver = uc.Chrome(options=options, use_subprocess=True)
+class Main:
+    def __init__(self):
+        self.driver = uc.Chrome(options=options)
 
-    def main(self):
-        self.driver.get("https://zefoy.com/")
+        while len(self.driver.window_handles) < 2:
+            sleep(1)
 
-        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, identifiers["views"])))
-        self.driver.find_element(By.CLASS_NAME, identifiers["views"]).click()
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        self.driver.close()
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        self.driver.get("https://zefoy.com")
+        sleep(10)
 
-        WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.XPATH, identifiers["urlBox"])))
-        self.driver.find_element(By.XPATH, identifiers["urlBox"]).send_keys(self.url)
 
-        self.driver.find_element(By.XPATH, identifiers["urlButton"]).click()
-
-        while True:
-            try:
-                WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, identifiers["submitButton"])))
-                sleep(1)
-                self.driver.find_element(By.XPATH, identifiers["submitButton"]).click()
-            except:
-                self.driver.find_element(By.XPATH, identifiers["urlButton"]).click()
-                    
-
-Zefoy("https://www.tiktok.com/@meepkid69/video/7048179708994800901").main()
+Main()
